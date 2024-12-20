a12cbe749b37b914a79500460d075087927f70f9
camilodev-ctrl <juancamilog0512@gmail.com>
2024-12-20 11:58:51 -0500

    Integrate streamlit in Synapse

--- src/app.py
+++ src/app.py
@@ -24,3 +24,28 @@ load_dotenv()
 
 # print("Bot: " + responseMessage)
 
+st.title("Synapse GPT")
+st.write("El mejor chatbot de la historia basado en GPT-3.5")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+st.session_state["messages"] = [{"role": "assistant", "content": "Hola, en que puedo ayudarte?"}]
+for message in st.session_state["messages"]:
+   st.chat_message(message["role"]).write(message["content"])
+
+
+
+
+if user_input := st.chat_input():
+    st.session_state["messages"].append({"role": "user", "content": user_input})
+    st.chat_message("user").write(user_input)
+    response = op.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages= st.session_state["messages"]
+    )
+    responseMessage = response['choices'][0]['message']['content']
+    st.session_state["messages"].append({"role": "assistant", "content": responseMessage})
+    st.chat_message("assistant").write(responseMessage)
+
+
